cdab000e3b6c854d32c875f8e8cef36bd57dacae
Kynan <k.song@blueyonder.co.uk>
2017-12-06 20:00:58 +0000

    Passed the second test.

--- array_tasks.js
+++ array_tasks.js
@@ -1,35 +1,36 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
+	concat: function (arr1, arr2) {
+		return arr1.concat(arr2)
+	},
 
-	// },
-
-	// insertAt: function (arr, itemToAdd, index) {
-		
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd );
+		return arr;
+	},
 
 	// square: function (arr) {
-		
+
 	// },
 
 	// sum: function (arr) {
-		
+
 	// },
 
 	// findDuplicates: function (arr) {
-		
+
 	// },
 
 	// removeAndClone: function (arr, valueToRemove) {
-		
+
 	// },
 
 	// findIndexesOf: function (arr, itemToFind) {
-		
+
 	// },
 
 	// sumOfAllEvenNumbersSquared: function (arr) {
-		
+
 	// }
 
 }
--- tests.js
+++ tests.js
@@ -3,14 +3,14 @@ var arrayTasks = require('./array_tasks.js')
 
 describe('Array tasks', function () {
 
-	xit('should concatenate two arrays, returning a new array', function () {
+	it('should concatenate two arrays, returning a new array', function () {
 		var arr1 = [1, 2, 3]
 		var arr2 = [4, 5, 6]
 		var expectation = [1, 2, 3, 4, 5, 6]
 		assert.deepStrictEqual(arrayTasks.concat(arr1, arr2), expectation)
 	})
 
-	xit('should insert an item in an array at any index position', function () {
+	it('should insert an item in an array at any index position', function () {
 		var arr = [1, 2, 4]
 		assert.deepStrictEqual(arrayTasks.insertAt(arr, 3, 2), [1, 2, 3, 4])
 	})
